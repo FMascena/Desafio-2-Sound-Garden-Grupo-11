68a1883ebf8abe75db0985f0d0b647fbb5918a63
Tiago Xavier <tiagocorx@gmail.com>
2023-02-11 22:53:06 -0300

    fix: date handling

--- js/admin.js
+++ js/admin.js
@@ -28,7 +28,7 @@ function convertDateFormat(jsonDate) {
   date = new Date(jsonDate);
   return (
     `${date.getDate().toString().padStart(2, "0")}/` +
-    `${date.getMonth().toString().padStart(2, "0")}/` +
+    `${(parseInt(date.getMonth()) + 1).toString().padStart(2, "0")}/` +
     `${date.getFullYear()} ` +
     `${date.getHours().toString().padStart(2, "0")}:` +
     `${date.getMinutes().toString().padStart(2, "0")}`
--- js/cadastro-evento.js
+++ js/cadastro-evento.js
@@ -35,10 +35,8 @@ function validateFormData(
   for (const field of arguments) {
     if (field.length === 0) throw new Error("Empty field");
   }
-  const dateRegex = /^\d{2}\/\d{2}\/\d{4}\s*\d{2}:\d{2}$/;
-  if (!dateRegex.test(scheduled)) throw new Error("invalid Date");
   attractions = attractions.split(",").map((e) => e.trim());
-  scheduled = new Date(scheduled).toJSON();
+  scheduled = validateDate(scheduled);
   number_tickets = parseInt(number_tickets, 10);
   return {
     name,
@@ -48,6 +46,14 @@ function validateFormData(
     scheduled,
     number_tickets,
   };
+}
+
+function validateDate(date) {
+  const dateRegex =
+    /^([0-2][0-9]|(3)[0-1])(\/)(([0-9])|((0)[0-9])|((1)[0-2]))(\/)\d{4}(\s*)(([0-1])?([0-9])|2([0-3])):([0-5])([0-9])$/;
+  if (!dateRegex.test(date)) throw new Error("invalid Date");
+  const [day, month, year, hour, minutes] = date.split(/[\/,\s*,:]/);
+  return new Date(year, parseInt(month) - 1, day, hour, minutes).toJSON();
 }
 
 async function sendNewEvent(formData) {
